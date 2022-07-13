bc6a0d678f8f0b81eb8c71b11689143e699d0f7c
yeze2 <102505362+yeze2@users.noreply.github.com>
2022-07-13 16:37:02 +0900

    [ 0713/develop ] - get NPC Set Class and Gardener Class

---
+++ Assets/Scripts/Gardener.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gardener : NPCSet
+{
+    public GameObject gardener;
+    Animator animator;
+    public SkinnedMeshRenderer smr;
+
+    public override void Detect()
+    {
+        base.Detect();
+    }
+    public override void DoWork()
+    {
+        base.DoWork();
+    }
+}
---
+++ Assets/Scripts/NPCSet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSet : MonoBehaviour 
+{
+
+    public virtual void DoWork()
+    {
+    }
+    public virtual void Detect()
+    {
+    }
+
+}
